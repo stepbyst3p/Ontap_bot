bcd580da5c082993f2c7a4cf387409d3625a1ec9
ivesergeev <ivesergeev@gmail.com>
2017-11-27 21:37:11 +0300

    Added message if bar.beers is empty

--- Bot/index.js
+++ Bot/index.js
@@ -68,22 +68,27 @@ bot.on("location", msg => {
               "http://localhost:8000/beers",
               { form: { barTitle: answer.text } },
               function(error, response, body) {
-                console.log(JSON.parse(body));
-                const prettyBeerList = _.map(
-                  JSON.parse(body),
-                  (beer, title) => {
-                    console.log({ beer });
-                    return `${emoji.beer} ${beer.title}\nПивоварня: ${
-                      beer.brewery
-                    }\nСтиль: ${beer.style}\nАлкоголь: ${beer.alc}`;
-                  }
-                );
-                console.log(prettyBeerList);
-                bot.sendMessage(
-                  chatId,
-                  prettyBeerList.join("\n\n"),
-                  markDownOption
-                );
+                if (!error) {
+                  console.log(JSON.parse(body));
+                  const prettyBeerList = _.map(
+                    JSON.parse(body),
+                    (beer, title) => {
+                      console.log({ beer });
+                      return `${emoji.beer} ${beer.title}\nПивоварня: ${
+                        beer.brewery
+                      }\nСтиль: ${beer.style}\nАлкоголь: ${beer.alc}%`;
+                    }
+                  );
+                  console.log(prettyBeerList);
+                  bot.sendMessage(
+                    chatId,
+                    prettyBeerList.join("\n\n"),
+                    markDownOption
+                  );
+                } else {
+                  bot.sendMessage(chatId, "Этот бар не опубликовал");
+                  console.log(error);
+                }
               }
             );
           });
