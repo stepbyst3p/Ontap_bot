d4a0b553f5cd2845c14891e4aef93690aaebbe5d
ivesergeev <ivesergeev@gmail.com>
2017-12-10 19:34:52 +0300

    removed error

--- Admin/src/components/Registration.js
+++ Admin/src/components/Registration.js
@@ -27,7 +27,7 @@ class Registration extends Component {
     debugger;
     fetch("/registration", {
       method: "POST",
-      headers: { 'content-type': 'application/json' },
+      headers: { "content-type": "application/json" },
       body: {
         name,
         email,
@@ -36,14 +36,17 @@ class Registration extends Component {
         barAddress
       }
     }).then(result => {
-      this.toaster.show({
-        intent: Intent.SUCCESS,
-        message:
-          "Заявка на регистрацию успешно отправлена. Мы свяжемся с вами, как только проверим все данные"
-      })
-      }).then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((err) => { console.log(err); });
+      this.toaster
+        .show({
+          intent: Intent.SUCCESS,
+          message:
+            "Заявка на регистрацию успешно отправлена. Мы свяжемся с вами, как только проверим все данные"
+        })
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(err => {
+          console.log(err);
+        });
       this.setState({
         requestStatus: "sended"
       });
